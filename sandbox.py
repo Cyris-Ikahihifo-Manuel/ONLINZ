bd5bd8356477b51fa9d5b7409874af9883f12c58
ikahih.cyris <ikahih.cyris@awatapu.school.nz>
2022-07-06 12:24:56 +1200

    the changes ive made as of 6/7/22

--- sandbox.py
+++
@@ -1,9 +0,0 @@
-base_multi = {'north island': 1, 'south island': 1.5, 'stewart island': 2}
-
-money = 8
-answer = input('enter input')
-for i in base_multi.keys():
-    if i == answer:
-        money *= base_multi[i]
-
-base_multi.update( )
