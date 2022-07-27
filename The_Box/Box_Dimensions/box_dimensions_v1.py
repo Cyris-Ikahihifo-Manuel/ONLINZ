85d7e4b1855b3025a2d84cef7d937f3ccc033d18
ikahih.cyris <ikahih.cyris@awatapu.school.nz>
2022-07-28 11:58:36 +1200

    work done as of 28.7.22

--- The_Box/Box_Dimensions/box_dimensions_v1.py
+++ The_Box/Box_Dimensions/box_dimensions_v1.py
@@ -1,5 +1,7 @@
-# box_dimension function 1.0 should crash for invalid values (such as strings or floats) and display no other output
+# box_dimension function 1.0 should crash for invalid values (such as strings or floats) and displays no other output
 # for out of bound values (-1 or 101). It also shouldn't reiterate the lines of code within the function.
+# Also the input from the user should be returned if the user has entered an integer equal to or more than 5 or equal
+# to or less than 100.
 
 
 def box_dimension(message):
@@ -16,4 +18,4 @@ depth = box_dimension('enter box\'s depth in cm')
 
 # the box's dimensions being output
 
-print('the box is {} cm tall, {} cm wide and {} cm thick')
+print('the box is {} cm tall, {} cm wide and {} cm thick'.format(height, width, depth))
